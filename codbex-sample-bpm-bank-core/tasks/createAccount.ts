45db2b06987637451868041786734fc8e05315f8
ThuF <jordan.p.pavlov@gmail.com>
2026-01-06 11:51:41 +0000

    Adds createAccount task

---
+++ codbex-sample-bpm-bank-core/tasks/createAccount.ts
@@ -0,0 +1,29 @@
+import { Process } from '@aerokit/sdk/bpm';
+import { UUID } from '@aerokit/sdk/utils';
+import { AccountRepository } from 'codbex-sample-edm-bank-core/gen/bankCore/data/accounts/AccountRepository';
+import { Tracer } from '../utils/Tracer';
+
+
+const tracer = new Tracer();
+
+try {
+    const customerId: number = Process.getExecutionContext().getVariable('customerId');
+
+    const repository = new AccountRepository();
+
+    const accountId = repository.create({
+        customerId: customerId,
+        iban: UUID.random().replace(/-/g, '').substring(0, 34),
+        balance: 1000.00,
+        // currency defaults to EUR
+        // status defaults to 1
+        // overdraftLimit defaults to 0
+    });
+
+    Process.getExecutionContext().setVariable('accountId', accountId);
+
+    tracer.complete(`Account created with id=[${accountId}].`);
+} catch (e: any) {
+    tracer.fail(e.message);
+    throw e;
+}
